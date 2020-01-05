aad64c3f8e3850c46d598269bc0ecb3b8f66184f
“Aria <a.c.nikzad@gmail.com>
2020-01-05 14:26:20 -0800

    effed

--- app/controllers/api/skills_controller.rb
+++ app/controllers/api/skills_controller.rb
@@ -1,2 +1,42 @@
 class Api::SkillsController < ApplicationController
 end
+
+
+  def index
+    @experiences = Experience.all
+    render 'index.json.jb'
+  end
+
+  def show
+    experience_id = params [:id]
+    @experience = Experience.find_by(id:experience_id)
+    render 'show.json.jb'
+  end
+
+  def create 
+    @experience = Experience.new(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company: params[:comapny],
+      details: params[:details]
+      )
+    if experience.save
+      render 'show.json.jb'
+      else
+        render json: {errors: @experience.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update 
+    @experience = Experience.find_by(id: params[:id])
+
+    @experience.update(
+      start_date: params[:start_date] || @experience.start_date,
+      end_date: params[:end_date] || @experience.end_date,
+      job_title: params[:job_title] || @experience.job_title,
+      company: params[:company] || @experience.company,
+      details: params[:details] || @experience.details,
+      )
+    render 'show.json.jb'
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
 
 
     #expererience 
-    get "/experience" => "experiences#index"
-    get "/experience/:id" => "experiences#show"
-    post "/experience" => "experiences#create"
-    # patch "/experience" => "experiences #update"
+    get "/experience" => "work_experiences#index"
+    get "/experience/:id" => "work_experiences#show"
+    post "/experience" => "work_experiences#create"
+    patch "/experience" => "work_experiences #update"
 
 
   end
