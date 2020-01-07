45f3dffdc8687ff2a65d23980790f59285d0d9dc
“Aria <a.c.nikzad@gmail.com>
2020-01-06 18:18:58 -0800

    finished skills actions

--- app/controllers/api/experience_controller.rb
+++ app/controllers/api/experience_controller.rb
@@ -42,17 +42,4 @@ class Api::ExperienceController < ApplicationController
     @experience.destroy
     render 'destroy.json.jb'
   end
-
-  def update 
-    @experience = Experience.find_by(id: params[:id])
-
-    @experience.update(
-      start_date: params[:start_date] || @experience.start_date,
-      end_date: params[:end_date] || @experience.end_date,
-      job_title: params[:job_title] || @experience.job_title,
-      company: params[:company] || @experience.company,
-      details: params[:details] || @experience.details,
-      )
-    render 'show.json.jb'
-  end
 end
--- app/controllers/api/skills_controller.rb
+++ app/controllers/api/skills_controller.rb
@@ -1,42 +1,38 @@
 class Api::SkillsController < ApplicationController
-end
-
 
   def index
-    @experiences = Experience.all
+    @skills = Skill.all
     render 'index.json.jb'
   end
 
   def show
-    experience_id = params [:id]
-    @experience = Experience.find_by(id:experience_id)
+    the_id = params[:id]
+    @skill = Skill.find_by(id:the_id)
     render 'show.json.jb'
   end
 
   def create 
-    @experience = Experience.new(
-      start_date: params[:start_date],
-      end_date: params[:end_date],
-      job_title: params[:job_title],
-      company: params[:comapny],
-      details: params[:details]
+    @skill = Skill.new(
+    skill_name: params[:skill_name],
+    student_id: params[:student_id]
       )
-    if experience.save
-      render 'show.json.jb'
-      else
-        render json: {errors: @experience.errors.full_messages}, status: :unprocessable_entity
-    end
+    @skill.save!
+    render 'show.json.jb'
   end
 
   def update 
-    @experience = Experience.find_by(id: params[:id])
+    @skill = Skill.find_by(id: params[:id])
 
-    @experience.update(
-      start_date: params[:start_date] || @experience.start_date,
-      end_date: params[:end_date] || @experience.end_date,
-      job_title: params[:job_title] || @experience.job_title,
-      company: params[:company] || @experience.company,
-      details: params[:details] || @experience.details,
+    @skill.update(
+      skill_name: params[:skill_name] || @skill.skill_name,
+      student_id: params[:student_id] || @skill.student_id
       )
     render 'show.json.jb'
   end
+
+  def destroy
+    @skill = Skill.find_by(id: params[:id])
+    @skill.destroy
+    render 'destroy.json.jb'
+  end
+end
